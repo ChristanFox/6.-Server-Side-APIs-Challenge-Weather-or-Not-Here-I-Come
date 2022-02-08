d37dae74ea1ca9ae21048d39fd9f6b4f649ff0f3
christanfox <bayshorestinks@aol.com>
2022-02-08 17:58:26 -0500

    requestURL function working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
-fetch(
-    "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=fddfd31365b535165b81caa3c50f62c5/?results=6"
-    )
+var searchBtnEl = document.getElementById("searchBtn");
+var cityNameEl = document.querySelector("#searchText");
+
+var getWeather = function() {
+    var cityName = cityNameEl.value;
+    var APIKey = "fddfd31365b535165b81caa3c50f62c5"
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    fetch(requestURL)
+      .then(function(response) {
+         return response.json();
+         })
+        .then(function(data) {
+            console.log(data);
+                console.log(data.name);
+                console.log(data.main.temp);
+                console.log(data.main.humidity);        
+        });
+}
+
+searchBtnEl.addEventListener("click", getWeather);
+
+
+      
